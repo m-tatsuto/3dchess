ae2f06f8d042b03576f455db949a53f6341e76c2
Tatsuto MAETSU <m.tatsuto@gmail.com>
2013-05-18 21:37:26 +0900

    rm main_test_js

--- comas/main_test_js/gameStratTest.js
+++
@@ -1,9 +0,0 @@
-function gameStartTest(){
-	turncamp = 0;
-	touchComaObject = null;
-	selectStockFlag = false;
-	setInitMapObject();
-	testMapDisplay();
-	setThreeJs3dMapObject();
-	window.scrollTo(0,1700);
-}
--- comas/main_test_js/moveTestThreeJs.js
+++
@@ -1,8 +0,0 @@
-function movePosition( mpx, mpy, mpz ){
-  var positionX = mpx;
-  var positionY = mpy;
-  var positionZ = mpz;
-  document.getElementById("positionValue").innerHTML = "";
-  var selobj = moveComaObject( mpx, mpy, mpz );
-  testMapDisplay();
-}
